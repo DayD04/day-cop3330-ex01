1b4a789f3ffd6017264fc51b0955b9b803f9ae99
tripp <davidaday4@gmail.com>
2021-09-06 17:53:34 -0400

    updated

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
+import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 David Day
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner input = new Scanner(System.in);
+        System.out.print( "What is your name?" );
+        String theName = input.nextLine();
+        System.out.println("Hello, " + theName + ", nice to meet you!");
+
     }
 }
